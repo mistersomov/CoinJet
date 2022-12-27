e18256103cc3d77b454c1587f2753610f54fca75
Ivan Somov <msijoc@gmail.com>
2022-12-27 23:54:45 +0400

    integrated room database

---
+++ app/src/main/java/com/mistersomov/coinjet/data/database/dao/CoinDao.kt
@@ -0,0 +1,24 @@
+package com.mistersomov.coinjet.data.database.dao
+
+import androidx.room.*
+import com.mistersomov.coinjet.data.database.entity.CoinEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CoinDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(coinList: List<CoinEntity>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertByEntity(entity: CoinEntity)
+
+    @Query("SELECT * from ${CoinEntity.TABLE_COIN_NAME} ORDER BY mktCap DESC")
+    fun getAll(): Flow<List<CoinEntity>>
+
+    @Query("SELECT * from ${CoinEntity.TABLE_COIN_NAME} WHERE id == :id LIMIT 1")
+    fun getById(id: String): Flow<CoinEntity>
+
+    @Query("DELETE from ${CoinEntity.TABLE_COIN_NAME}")
+    suspend fun deleteAll()
+}
